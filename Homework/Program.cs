764537bcf8f7196bdd4300076811c0b82b8dd742
unknown <nellysherova@mail.ru>
2021-01-21 23:35:31 +0300

    Complete homework

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -6,7 +6,7 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            /*{
+            {
                 Console.WriteLine("ДЗ 1 задание 2");
                 Console.Write("A = "); int.TryParse(Console.ReadLine(), out int a);
                 Console.Write("B = "); int.TryParse(Console.ReadLine(), out int b);
@@ -42,7 +42,7 @@ namespace Homework
                         Console.WriteLine("Error Вы ввели неправильный sign");
                         break;
                 }
-            } */
+            } 
             {
                 Console.WriteLine("ДЗ 1 задание 4");
                 Console.Write("Count = "); int.TryParse(Console.ReadLine(), out int count);
@@ -51,6 +51,74 @@ namespace Homework
                 else if (count >= 36 && count <= 50) Console.WriteLine("Вы попали в отрезок [36 - 50]");
                 else if (count >= 51 && count <= 100) Console.WriteLine("Вы попали в отрезок [51 - 100]");
                 else Console.WriteLine("Вы не попали не на один отрезок!");
+            }
+            {
+                Console.WriteLine("ДЗ 2 задание 2");
+                Console.Write("Cost = "); double.TryParse(Console.ReadLine(), out double cost);
+                if (cost > 500 && cost <= 1000) cost  = cost  - ((cost * 3) / 100);
+                else if (cost > 1000) cost = cost - ((cost * 5) / 100);
+                else if (cost < 0) Console.WriteLine("Сумма покупки не может быть отрицательным числом!");
+                if (cost > 0) Console.WriteLine($"Стоимость вашей покупки с учетом скидки: {cost}");
+            }
+            {
+                Console.WriteLine("ДЗ 2 задание 3");
+                Console.Write("num1 = "); double.TryParse(Console.ReadLine(), out double num1);
+                Console.Write("num2 = "); double.TryParse(Console.ReadLine(), out double num2);
+                Console.Write("num3 = "); double.TryParse(Console.ReadLine(), out double num3);
+                Console.Write("num4 = "); double.TryParse(Console.ReadLine(), out double num4);
+                var min = num1;
+                if (num1 > num2 || num2 > num3 || num3 > num4)
+                {
+                    if (num2 < min ) min = num2;
+                    else if (num3 < min) min = num3;
+                    else if (num4 < min) min = num4;
+                    Console.WriteLine($"Min is {min}");
+                }
+                else if (num1 == num2  && num2 == num3 && num3 == num4) Console.WriteLine(num1 * num2 * num3 * num4);
+                else Console.WriteLine("Числа расположены по возрастанию!");
+            }
+            {
+                Console.WriteLine("ДЗ 2 задание 4");
+                Console.Write("a = "); double.TryParse(Console.ReadLine(), out double a);
+                Console.Write("b = "); double.TryParse(Console.ReadLine(), out double b);
+                Console.Write("c = "); double.TryParse(Console.ReadLine(), out double c);
+                if (a >= c && c >= b) 
+                {
+                    b = b + c; 
+                    c = b - c; 
+                    b = b - c;
+                }
+                else if (b >= a && a >= c) 
+                {
+                    a = a + b; 
+                    b = a - b; 
+                    a = a - b;
+                }
+                else if (c >= b && b >= a)
+                {
+                    c = a + c;
+                    a = c - a;
+                    c = c - a;
+                }
+                else if (c >= a && a >= b)
+                {
+                    c = c + a;
+                    a = c - a;
+                    c = c - a;
+                    c = c + b;
+                    b = c - b;
+                    c = c - b;
+                }
+                else if (b >= c && c >= a)
+                {
+                    c = c + a;
+                    a = c - a;
+                    c = c - a;
+                    b = b + a;
+                    a = b - a;
+                    b = b - a;
+                }
+                Console.WriteLine($"{a} >= {b} >= {c}");
             }
         }
     }
